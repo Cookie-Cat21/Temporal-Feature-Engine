8084d10282ab9cf43645598d29b5c9844e80bc17
Cookie-Cat21 <201578028+Cookie-Cat21@users.noreply.github.com>
2026-03-29 08:23:16 +0530

    chore: Remove CommitGlow and restructure repo for Temporal Engine

--- CommitGlow/background.js
+++
@@ -1,5 +0,0 @@
-chrome.action.onClicked.addListener((tab) => {
-  chrome.tabs.sendMessage(tab.id, { action: "toggle" }).catch(err => {
-    console.log("Cannot send message to this page: ", err.message);
-  });
-});
